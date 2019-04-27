c6b340f29ad67ac502e2341dad7d7fc8d114ec05
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2019-04-27 18:15:26 -0300

    Primeiro quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,5 @@
+import { resolve } from "path";
+
 /*
 Instructions:
 (1) Wrap an XHR in a Promise in the get() function below. See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
@@ -36,23 +38,37 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
+
+    debugger;
     var req = new XMLHttpRequest();
     req.open('GET', url);
     req.onload = function() {
       if (req.status === 200) {
         // It worked!
         // You'll want to resolve with the data from req.response
+
+        resolve();
+
+        debugger;
+
       } else {
         // It failed :(
         // Be nice and reject with req.statusText
+
+        debugger;
       }
     };
+
+    
     req.onerror = function() {
       // It failed :(
       // Pass a 'Network Error' to reject
+
+      debugger;
     };
     req.send();
   }
+
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
@@ -61,6 +77,6 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results.json')
   });
 })(document);
